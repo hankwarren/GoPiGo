31c177120f9900ed012e7c4ae294fddd9692844c
Hank Warren <hankwarren@comcast.net>
2017-07-23 05:58:11 +0000

    added comands for streaming

--- server.sh
+++ server.sh
@@ -1,6 +1,7 @@
-MJPG=/usr/local/lib
+MJPG=/opt/mjpg-streamer
 echo $MJPG
-LD_LIBRARY_PATH=$MJPG/ mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"
+raspistill -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 -n &
+LD_LIBRARY_PATH=$MJPG/ /opt/mjpg-streamer/mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" &
 
 # To look at the network stuff (ports and such)
 #   sudo lsof -i
